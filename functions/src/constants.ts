69761067e44ea7469bb1d91b94d594fa75559068
John Roice Aldeza <jroicealdeza@gmail.com>
2025-12-06 19:55:32 +0800

    style: format code for consistency and readability; add Firebase Messaging service worker

--- functions/src/constants.ts
+++ functions/src/constants.ts
@@ -1,4 +1,4 @@
 export const TOPIC = "alerts";
 // Notification cooldown period
-export const NOTIFICATION_COOLDOWN_MS = 10 * 1000; // 10 seconds (testing)
-// export const NOTIFICATION_COOLDOWN_MS = 10 * 60 * 1000; // 10 minutes (production)
+export const NOTIFICATION_COOLDOWN_MS = 10 * 1000;
+// export const NOTIFICATION_COOLDOWN_MS = 10 * 60 * 1000;
---
+++ web-app/public/firebase-messaging-sw.js
@@ -0,0 +1,38 @@
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here. Other Firebase libraries
+// are not available in the service worker.
+// Replace 10.13.2 with latest version of the Firebase JS SDK.
+importScripts(
+  "https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js"
+);
+importScripts(
+  "https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js"
+);
+
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
+firebase.initializeApp({
+  apiKey: "AIzaSyB2MTe7Dgrl0NC2eUBDdzjikaLO_bYJYV4",
+  authDomain: "aquarium-monitoring-bd878.firebaseapp.com",
+  databaseURL:
+    "https://aquarium-monitoring-bd878-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  projectId: "aquarium-monitoring-bd878",
+  storageBucket: "aquarium-monitoring-bd878.firebasestorage.app",
+  messagingSenderId: "1037054047078",
+  appId: "1:1037054047078:web:a453f7f04021b85f808ea7",
+  measurementId: "G-5GT6ELH2CQ",
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    payload
+  );
+
+  self.registration.showNotification(payload);
+});
